0f2f7fdcad127fc1c6e3baa803cab1d357c64e65
Nick Scheibenpflug <zorchenhimer@gmail.com>
2015-12-16 16:06:46 -0500

    Make FileInfo class better
    * Remove dashes from hashes.
    * Add FileString() for easier printing of info to file.

--- FileInfo.cs
+++ FileInfo.cs
@@ -6,15 +6,40 @@ using System.Threading.Tasks;
 
 namespace HashUtil {
     class FileInfo {
+
+        private string _name;
+        private string _md5;
+        private string _crc32;
+        private string _sha1;
+
         public string Name { get; set; }
-        public string MD5 { get; set; }
-        public string CRC32 { get; set; }
-        public string SHA1 { get; set; }
+        public string MD5   { get { return _md5; }
+            set {
+                if (value == null)
+                    return;
+                _md5 = value.Replace("-", "");
+            } }
+        public string CRC32 { get { return _crc32; }
+            set {
+                if (value == null)
+                    return;
+                _crc32 = value.Replace("-", "");
+            } }
+        public string SHA1  { get { return _sha1; }
+            set {
+                if (value == null)
+                    return;
+                _sha1 = value.Replace("-", "");
+            } }
 
         public FileInfo() {}
 
         public FileInfo(string name) {
             this.Name = name;
+        }
+
+        public string FileString() {
+            return SHA1 + " " + MD5 + " " + CRC32 + " " + Name;
         }
     }
 }
